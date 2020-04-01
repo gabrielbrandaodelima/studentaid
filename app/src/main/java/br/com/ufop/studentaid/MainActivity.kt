ebf182040fa91fbfa719e70847b7f1e5ff472205
Gabriel B. Lima <gabriel.brandao@usemobile.xyz>
2020-04-01 20:49:42 -0300

    #Feat: Configured app_bar_searchable.xml

--- app/src/main/java/br/com/ufop/studentaid/MainActivity.kt
+++ app/src/main/java/br/com/ufop/studentaid/MainActivity.kt
@@ -5,5 +5,4 @@ import br.com.ufop.studentaid.core.platform.BaseActivity
 class MainActivity : BaseActivity(R.layout.main_activity) {
 
 
-
 }
--- app/src/main/java/br/com/ufop/studentaid/core/platform/BaseActivity.kt
+++ app/src/main/java/br/com/ufop/studentaid/core/platform/BaseActivity.kt
@@ -1,8 +1,22 @@
 package br.com.ufop.studentaid.core.platform
 
+import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import br.com.ufop.studentaid.R
+import kotlinx.android.synthetic.main.app_bar_layout.*
 
 abstract class BaseActivity(@LayoutRes contentLayoutId: Int = 0) : AppCompatActivity(contentLayoutId) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        toolbar_main?.let{
+
+            setSupportActionBar(it)
+        }
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+    }
 
 }
