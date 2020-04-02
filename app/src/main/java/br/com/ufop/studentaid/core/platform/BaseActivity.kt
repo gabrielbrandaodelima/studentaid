ace7e7b3a348ecb6b3b98ea1473ce64b09292973
Gabriel B. Lima <gabriel.brandao@usemobile.xyz>
2020-04-01 21:11:07 -0300

    #Feat: Configured app_bar_searchable.xml

--- app/src/main/java/br/com/ufop/studentaid/core/platform/BaseActivity.kt
+++ app/src/main/java/br/com/ufop/studentaid/core/platform/BaseActivity.kt
@@ -1,16 +1,22 @@
 package br.com.ufop.studentaid.core.platform
 
 import android.os.Bundle
+import android.os.Handler
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import br.com.ufop.studentaid.R
 import kotlinx.android.synthetic.main.app_bar_layout.*
+import kotlinx.android.synthetic.main.main_activity.*
 
-abstract class BaseActivity(@LayoutRes contentLayoutId: Int = 0) : AppCompatActivity(contentLayoutId) {
+abstract class BaseActivity(@LayoutRes contentLayoutId: Int = 0) :
+    AppCompatActivity(contentLayoutId) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        toolbar_main?.let{
+        toolbar_main?.let {
 
             setSupportActionBar(it)
         }
@@ -19,4 +25,65 @@ abstract class BaseActivity(@LayoutRes contentLayoutId: Int = 0) : AppCompatActi
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            android.R.id.home -> handleDrawer()
+
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+
+    var doubleBackToExit: Boolean = false
+
+    private fun handleDrawer() {
+        drawer_layout?.let {
+            if (it.isDrawerOpen(GravityCompat.START))
+                drawer_layout?.closeDrawer(GravityCompat.START)
+            else {
+                drawer_layout?.openDrawer(GravityCompat.START)
+
+            }
+
+        }
+
+    }
+
+    private fun handleBackPressed() {
+
+        showMessage("Pressione novamente para sair")
+        this.doubleBackToExit = true
+    }
+
+    /**
+     * - Display a Toasty message
+     * @param message String msg
+     * @param isErrorMessage set to true if is an error
+     */
+    fun showMessage(message: String, isErrorMessage: Boolean = false) {
+
+//        if (isErrorMessage) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+//            Toasty.error(this, message).show()
+//        } else {
+
+//            Toasty.success(this, message).show()
+    }
+
+    override fun onBackPressed() {
+        drawer_layout?.let {
+            if (it.isDrawerOpen(GravityCompat.START))
+                drawer_layout?.closeDrawer(GravityCompat.START)
+            else {
+
+                when {
+                    doubleBackToExit -> moveTaskToBack(true)
+                    else -> handleBackPressed()
+                }
+                Handler().postDelayed({ doubleBackToExit = false }, 2000)
+            }
+
+        }
+    }
 }
