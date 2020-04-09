bf0666c8e945b8687910622fc8b723b4075ad6a5
Gabriel B. Lima <gabriel.brandao@usemobile.xyz>
2020-04-09 07:50:27 -0300

    #Feat: Setup Google Maps

--- app/src/main/java/br/com/ufop/studentaid/features/MainActivity.kt
+++ app/src/main/java/br/com/ufop/studentaid/features/MainActivity.kt
@@ -1,9 +1,42 @@
 package br.com.ufop.studentaid.features
 
+import android.os.Bundle
 import br.com.ufop.studentaid.R
 import br.com.ufop.studentaid.core.platform.BaseActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : BaseActivity(R.layout.main_activity) {
+class MainActivity : BaseActivity(R.layout.main_activity), OnMapReadyCallback {
 
+    private var googleMap: GoogleMap? = null
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        main_map?.let {
+            (it as SupportMapFragment).getMapAsync(this)
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap?) {
+        this.googleMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        this.googleMap?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        this.googleMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 }
