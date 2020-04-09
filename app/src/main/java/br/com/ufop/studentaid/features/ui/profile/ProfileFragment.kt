c57ed65340632e179a92e341bce9830d77ce217e
Gabriel B. Lima <gabriel.brandao@usemobile.xyz>
2020-04-09 17:26:44 -0300

    #Feat: Created profile_fragment.xml

---
+++ app/src/main/java/br/com/ufop/studentaid/features/ui/profile/ProfileFragment.kt
@@ -0,0 +1,33 @@
+package br.com.ufop.studentaid.features.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import br.com.ufop.studentaid.R
+
+class ProfileFragment : Fragment() {
+
+    companion object {
+        fun newInstance() =
+            ProfileFragment()
+    }
+
+    private lateinit var viewModel: ProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.profile_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/br/com/ufop/studentaid/features/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.ufop.studentaid.features.ui.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
