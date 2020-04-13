6c1a53ad3fd89c7fbaf690917fb491e456f741d7
Gabriel B. Lima <gabriel.brandao@usemobile.xyz>
2020-04-13 13:56:58 -0300

    #Feat: added map interactions

--- app/src/main/java/br/com/ufop/studentaid/features/ui/main/MainFragment.kt
+++ app/src/main/java/br/com/ufop/studentaid/features/ui/main/MainFragment.kt
@@ -1,6 +1,8 @@
 package br.com.ufop.studentaid.features.ui.main
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import br.com.ufop.studentaid.R
@@ -10,11 +12,13 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.main_fragment.*
+import java.util.*
 
 class MainFragment : BaseFragment(R.layout.main_fragment), OnMapReadyCallback {
-
+    private val TAG = this::class.java.simpleName
     private lateinit var viewModel: MainViewModel
 
     private var googleMap: GoogleMap? = null
@@ -75,15 +79,84 @@ class MainFragment : BaseFragment(R.layout.main_fragment), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap?) {
         this.googleMap = googleMap
+        googleMap?.apply {
+            setMapLongClick(this)
+            setPoiClick(this)
+            setMapStyle(this)
+
+        }
 //        googleMap?.mapType = GoogleMap.MAP_TYPE_TERRAIN
         // Add a marker in Sydney and move the camera
-        val pasargada = LatLng(-20.399039, -43.513923)
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap?.addMarker(MarkerOptions().position(pasargada).title("Republica Pasárgada"))
+//        val pasargada = LatLng(-20.399039, -43.513923)
+//        val sydney = LatLng(-34.0, 151.0)
+//        googleMap?.addMarker(MarkerOptions().position(pasargada).title("Republica Pasárgada"))
 //        this.googleMap?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
 //        this.googleMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        this.googleMap?.moveCamera(CameraUpdateFactory.newLatLng(pasargada))
+//        this.googleMap?.moveCamera(CameraUpdateFactory.newLatLng(pasargada))
 //        this.googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(pasargada,1.0F))
+
+        val latitude = -20.399077
+        val longitude = -43.514099
+
+        val homeLatLng = LatLng(latitude, longitude)
+
+        val zoomLevel = 15f
+
+        this.googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        this.googleMap?.addMarker(MarkerOptions().position(homeLatLng).title("Republica Pasárgada"))
+    }
+
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            // A Snippet is Additional text that's displayed below the title.
+            val snippet = String.format(
+                    Locale.getDefault(),
+                    "Lat: %1$.5f, Long: %2$.5f",
+                    latLng.latitude,
+                    latLng.longitude
+            )
+            map.addMarker(
+                    MarkerOptions()
+                            .position(latLng)
+                            .title("Dropped pin")
+                            .snippet(snippet)
+
+            )
+            map.addMarker(
+                    MarkerOptions()
+                            .position(latLng)
+            )
+        }
+    }
+
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                    MarkerOptions()
+                            .position(poi.latLng)
+                            .title(poi.name)
+            )
+            poiMarker.showInfoWindow()
+        }
+    }
+
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            val success = map.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            requireContext(),
+                            R.raw.map_style_standard
+                    )
+            )
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", e)
+        }
     }
 
 }
